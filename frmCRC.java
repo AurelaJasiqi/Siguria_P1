f7dbbd09ff38d4f160f3fba0ad3e6cac07eb3b05
zgjimkrasniqi <36892990+zgjimkrasniqi@users.noreply.github.com>
2018-05-11 08:06:26 +0200

    Create frmCRC.java

---
+++ frmCRC.java
@@ -0,0 +1,14 @@
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.Font;
+import java.awt.Color;
+import java.math.BigInteger;
