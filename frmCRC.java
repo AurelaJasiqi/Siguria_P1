f14109ea3ab17c9b8838c4d4ec6aed3ade448807
AurelaJasiqi <36892991+AurelaJasiqi@users.noreply.github.com>
2018-05-11 08:13:36 +0200

    Update frmCRC.java

--- frmCRC.java
+++ frmCRC.java
@@ -13,6 +13,44 @@ import java.awt.Font;
 import java.awt.Color;
 import java.math.BigInteger;
 
-dskjfhdfgndf
-dsgsfjofgih
-ghmofgihj
+public class frmCRC extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField txtText;
+	private JTextField textGjeneratori;
+	private JTextField txtTekstiIpranuar;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					frmCRC frame = new frmCRC();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public frmCRC() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		JLabel lblFjaliaDerguar = new JLabel("");
+		JLabel lblInfo = new JLabel("");
+		lblInfo.setForeground(Color.RED);
+		JLabel lblInput = new JLabel("Teksti qe doni te shkruani:");
+		lblInput.setFont(new Font("Tahoma", Font.BOLD, 12));
+		lblInput.setBounds(27, 26, 163, 14);
+		contentPane.add(lblInput);
+
